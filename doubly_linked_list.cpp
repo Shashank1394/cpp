9c5a4bcb9b9ac40b0b09840b4af881b8a1b39cb9
Shashank Jadhav <shashankjadhav13940@gmail.com>
2025-01-14 16:09:27 +0530

    dll doubled

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -17,7 +17,7 @@ void display();
 int main() {
   int ch;
   while (1) {
-    std::cout << "\n\n--- Doubly Linked List --";
+    std::cout << "\n\n--- Doubly Linked List ---";
     std::cout << "\n1. Create";
     std::cout << "\n2. Insert at beginning";
     std::cout << "\n3. Insert at last";
@@ -44,13 +44,11 @@ int main() {
       display();
       break;
     case 6:
-      return (0);
-      break;
+      return 0;
     default:
       std::cout << "Invalid choice!\n";
     }
   }
-  return 0;
 }
 
 void create() {
@@ -62,24 +60,25 @@ void create() {
   if (head == NULL) {
     head = newnode;
     std::cout << "Doubly linked list created successfully!\n";
-    return;
+  } else {
+    std::cout << "List already created. Use insert operations.\n";
   }
 }
 
 void insert_at_beginning() {
-  if (head == NULL) {
-    create();
-    return;
-  }
-
   node *newnode = new node;
   newnode->next = newnode->prev = NULL;
   std::cout << "\nEnter the data: ";
   std::cin >> newnode->data;
 
-  newnode->next = head;
-  head = newnode;
-  return;
+  if (head == NULL) {
+    head = newnode;
+  } else {
+    newnode->next = head;
+    head->prev = newnode;
+    head = newnode;
+  }
+  std::cout << "Inserted at the beginning.\n";
 }
 
 void insert_at_last() {
@@ -88,7 +87,7 @@ void insert_at_last() {
     return;
   }
 
-  struct node *curr = head;
+  node *curr = head;
   while (curr->next != NULL) {
     curr = curr->next;
   }
@@ -100,36 +99,36 @@ void insert_at_last() {
 
   curr->next = newnode;
   newnode->prev = curr;
+  std::cout << "Inserted at the last.\n";
 }
 
 void insert_in_between() {
   if (head == NULL) {
+    std::cout << "List is empty. Creating a new list...\n";
     create();
     return;
   }
 
-  int insert_here;
-  std::cout << "\nWhere do you wanna insert it: ";
-  std::cin >> insert_here;
+  int position;
+  std::cout << "\nEnter the position to insert: ";
+  std::cin >> position;
 
-  if (insert_here <= 1) {
+  if (position <= 1) {
     insert_at_beginning();
     return;
   }
 
   node *curr = head;
   int i = 1;
-  while (i < insert_here - 1 && curr->next != NULL) {
+  while (i < position - 1 && curr->next != NULL) {
     curr = curr->next;
     i++;
   }
 
-  if (i > insert_here) {
+  if (i < position - 1) {
     std::cout << "Position out of range. Inserting at the end.\n";
     insert_at_last();
     return;
-  } else {
-    std::cout << "Fool!";
   }
 
   node *newnode = new node;
@@ -138,18 +137,25 @@ void insert_in_between() {
   std::cin >> newnode->data;
 
   newnode->next = curr->next;
-  newnode->prev = curr;
-  curr->next->prev = newnode;
+  if (curr->next != NULL) {
+    curr->next->prev = newnode;
+  }
   curr->next = newnode;
+  newnode->prev = curr;
 
-  std::cout << "Inserted at position " << insert_here << ".\n";
+  std::cout << "Inserted at position " << position << ".\n";
 }
 
 void display() {
+  if (head == NULL) {
+    std::cout << "List is empty.\n";
+    return;
+  }
+
   node *curr = head;
   std::cout << "\nDoubly Linked List: ";
   while (curr != NULL) {
-    std::cout << curr->data << " -> ";
+    std::cout << curr->data << " <-> ";
     curr = curr->next;
   }
   std::cout << "NULL\n";
